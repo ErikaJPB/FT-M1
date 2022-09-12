034137630d3cc674459bfe2e90730cb956aff49a
ErikaJPB <erikajpinedab@gmail.com>
2022-09-12 14:31:47 -0500

    Intro to CS homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,12 +1,39 @@
 'use strict'
 
 function BinarioADecimal(num) {
+  
   // tu codigo aca
 
+
+let binario = String(num).split("").filter( i => i === '1' || i === "0")
+
+let resultado = 0;
+
+for (let i = 0; i < binario.length; i++) {
+  resultado = resultado + Number(binario[i] * Math.pow(2, binario.length - 1 - i) )
+}
+
+return resultado;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+let resultado = "";
+let divisor = 2;
+let cociente = 0;
+let residuo = 0;
+
+  while (num >= 2) {
+    cociente = Math.trunc(num / divisor);
+    residuo = num % divisor;
+    resultado += residuo.toString()
+    num = cociente;
+  }
+
+  let auxiliar = num.toString();
+  resultado += auxiliar;
+  return resultado.split("").reverse().join("");
+  
 
 }
 
